347c1686f619dc3ad9a1e106496e87457463744d
HelloFriend-99 <amirmahdixxpadash01@gmail.com>
2025-04-15 22:13:59 +0330

    Update script.js

--- script.js
+++ script.js
@@ -1,61 +1,33 @@
 const noButton = document.getElementById('noButton');
-const body = document.body;
+const yesButton = document.getElementById('yesButton');
 
-let moveCount = 0; 
 let clickCount = 0; 
 
-// array for quotes
 const messages = [
     "نهه نکن این کار رو!",
-    "لطفاا",
+    "🥲لطفاا",
+    "جاان من"
+    , "دارم عاشقت میشم"
+    , "🥺ای خدا نگاهش کن"
+    , "😄خیلی قشنگ نه میزنی"
+    , "🙄ولی دیگه نزن",
     "🙂توروخدا",
     "😶نمیبینی چی میگم؟",
     "🥺آره رو بزن لطفا",
     "😫عاشقتم لطفا آره رو بزن"
 ];
 
-
-function getRandomPosition() {
-    const maxX = body.offsetWidth - noButton.offsetWidth; 
-    const maxY = body.offsetHeight - noButton.offsetHeight; 
-
-    // تولید مختصات تصادفی
-    const randomX = Math.random() * maxX;
-    const randomY = Math.random() * maxY;
-
-    return { x: randomX, y: randomY };
-}
 function yesButtonMsg(){
-    alert("🤍😍آفرین دیگه منو تو زن و شوهریم")
+    alert("🤍🥲😍 آفرین مبینای قشنگم به قلب من خوش اومدی")
 }
-
-noButton.addEventListener('mouseover', () => {
-    if (moveCount < 10) {
-        moveCount++; 
-
-   
-        const randomPosition = getRandomPosition();
-
-
-        noButton.style.position = 'absolute';
-        noButton.style.left = randomPosition.x + 'px';
-        noButton.style.top = randomPosition.y + 'px';
-    }
-});
-
 
 noButton.addEventListener("click", () => {
-
-    if (moveCount >= 10) {
-        clickCount++; 
-
-
-        const messageIndex = clickCount % messages.length; 
-        alert(messages[messageIndex]);
-    }
+    clickCount++; 
+    const messageIndex = clickCount % messages.length; 
+    alert(messages[messageIndex]);
 });
 
-yesButton.addEventListener("click" , () => {
+yesButton.addEventListener("click", () => {
     yesButtonMsg()
-})
+});
 
